d603411bc37240a9ae4117854d509b2f7991dbbd
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-04-05 17:03:01 +0300

    fix the issue with base64 encoded svgs

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var base64Img = require('base64-img');
 hexo.extend.helper.register('inline_svg', function (filename, base64) {
   var path = pathFn.join(hexo.theme_dir, 'source', filename);
   if (base64) {
-    return base64Img.base64Sync(path);
+    return base64Img.base64Sync(path).replace('svg;', 'svg+xml;');
   }
   return fs.readFileSync(path);
 });
